cdf1faecffbf8c7b976eb1b8763f4587596ca3d5
Mohammad Rubel Rana <alimamu2024@gmail.com>
2026-04-28 09:39:53 +0300

    Fix supabase client with ssr

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,16 +1,18 @@
-import { createClient } from '@supabase/supabase-js';
+// lib/supabase/client.ts
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://nryqoyqdwxqdydifatzb.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yeXFveXFkd3hxZHlkaWZhdHpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxOTQyNzEsImV4cCI6MjA5Mjc3MDI3MX0.TLl1cJDhipmG4NczcG6kZUVEB7KAtbi6Rwis6lXH5GA';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-// ভ্যারিয়েবল আগে ডিক্লেয়ার করলাম যাতে লাল দাগ না আসে
-let supabaseInstance: any = null;
+export const createClient = () => {
+  return createBrowserClient(supabaseUrl, supabaseAnonKey)
+}
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+let clientInstance: ReturnType<typeof createBrowserClient> | null = null
 
-export const getSupabaseClient = () => {
-  if (!supabaseInstance) {
-    supabaseInstance = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = () => {
+  if (!clientInstance) {
+    clientInstance = createBrowserClient(supabaseUrl, supabaseAnonKey)
   }
-  return supabaseInstance;
-};
+  return clientInstance
+}
